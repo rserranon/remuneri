d7c174745f7ed3cadd53f0a734799b2a893e7ebd
rserranon <rserranon@gmail.com>
2021-07-21 19:37:44 -0500

    Added constrains and PublicoObjetivo Class

--- grails-app/domain/remuneri/Anuncio.groovy
+++ grails-app/domain/remuneri/Anuncio.groovy
@@ -1,17 +1,33 @@
 package remuneri
+// import grails.rest.*
+
+// @Resource(uri='/anuncios')
 
 class Anuncio {
-	String 	nombre
-	String 	descripcion
-	Integer monto
-	Float 	latitud
-	Float	longitud
-	Float 	radio
-	Date	de
-	Date	hasta
-	Boolean lunes
-	Date 	lunesDe
-	Date	lunesA
-    static constraints = {
+	String      nombre
+	String 	    descripcion
+	Integer     monto
+	BigDecimal 	latitud
+  BigDecimal  longitud
+	BigDecimal	radio
+	Date	      de
+	Date	      hasta
+	Boolean     lunes
+	Date 	      lunesDe
+	Date	      lunesA
+
+  static hasMany = [ publicos: PublicoObjetivo]
+
+  static constraints = {
+        nombre      size: 5..25, blank: false, unique: true
+        descripcion size: 5..35, blank: false
+        monto       min: 0
+        latitud     (min: new BigDecimal(-90.0), max: new BigDecimal(90.0)) 
+        longitud    (min: new BigDecimal(-180.0), max: new BigDecimal(180.0))
+        radio       (min: new BigDecimal(0.0), max: new BigDecimal(1000.0)) 
+        de          blank: false
+        hasta       blank: false
+        lunesDe     blank: false
+        lunesA      blank: false
     }
 }
---
+++ grails-app/domain/remuneri/PublicoObjetivo.groovy
@@ -0,0 +1,22 @@
+package remuneri
+
+class PublicoObjetivo {
+    String  nombre
+    Integer edadDe
+    Integer edadHasta
+    String  nse
+    String  sistemaOperativo
+    BigDecimal  latitud
+    BigDecimal  longitud
+    BigDecimal  radio
+    static constraints = {
+          nombre    size: 5..25, blank: false, unique: true
+          edadDe    min: 18, max: 99
+          edadHasta min: 18, max: 99
+          nse (inList:["A", "B", "C+", "C"])
+          sistemaOperativo (inList:["iOS", "Android", "Otro"])
+          latitud     (min: new BigDecimal(-90.0), max: new BigDecimal(90.0))
+          longitud    (min: new BigDecimal(-180.0), max: new BigDecimal(180.0))
+          radio       (min: new BigDecimal(0.0), max: new BigDecimal(1000.0))
+    }
+}
